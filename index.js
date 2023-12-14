5970625fb9629bb9c8a9efe1c62045928854344b
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-12-14 11:11:33 -0300

    create try catch validations on server

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 // import { writeFile, readFile } from "node:fs/promises";
 import fs from "fs";
 import cors from "cors";
-const app = express();
 
+const app = express();
 
 app.use(express.json());
 app.use(cors());
@@ -12,54 +12,63 @@ app.listen(5000, () => {
     console.log("Example app listening on port 5000");
 });
 
+app.get("/", (req, res) => {
+    try {
+        res.status(200).sendFile(__dirname + "/index.html");
+    } catch (error) {
+        res.status(404).sendFile({ error: "Error al procesar la solicitud" });
+        console.error("Error al procesar la solicitud:", error);
+    }
+});
+
 app.get("/canciones", (req, res) => {
-    const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
-    res.status(200).json(canciones)
-/* guiño */
-    res.status(500).json({ error: "Error al procesar la solicitud" })
-    console.error("Error al procesar la solicitud:", error)
+    try {
+        const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+        res.status(200).json(canciones);
+    } catch (error) {
+        res.status(500).json({ error: "Error al procesar la solicitud" });
+        console.error("Error al procesar la solicitud:", error);
+    }
 });
 
 app.post("/canciones", (req, res) => {
-    const cancion = req.body;
-    const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
-    canciones.push(cancion);
-    fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
-    res.status(200).send("Canción modificada con éxito")
- /* guiño guiño */
-    res.status(500).json({ error: "Error al procesar la solicitud" })
-    console.error("Error al procesar la solicitud:", error)
-})
+    try {
+        const cancion = req.body;
+        const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+        canciones.push(cancion);
+        fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
+        res.status(200).send("Canción modificada con éxito");
+    } catch (error) {
+        res.status(500).json({ error: "Error al procesar la solicitud" });
+        console.error("Error al procesar la solicitud:", error);
+    }
+});
 
 app.put("/canciones/:id", (req, res) => {
-    const { id } = req.params
-    const cancion = req.body
-    const canciones = JSON.parse(fs.readFileSync("repertorio.json"))
-    const index = canciones.findIndex(c => c.id == id)
-    canciones[index] = cancion
-    fs.writeFileSync("repertorio.json", JSON.stringify(canciones))
-    res.status(201).send("¡Canción agregada con éxito!")
-/* más guiños */
-    res.status(500).json({ error: "Error al procesar la solicitud" })
-    console.error("Error al procesar la solicitud:", error)
-})
+    try {
+        const { id } = req.params;
+        const cancion = req.body;
+        const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+        const index = canciones.findIndex((c) => c.id == id);
+        canciones[index] = cancion;
+        fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
+        res.status(201).send("¡Canción agregada con éxito!");
+    } catch (error) {
+        res.status(500).json({ error: "Error al procesar la solicitud" });
+        console.error("Error al procesar la solicitud:", error);
+    }
+});
 
 app.delete("/canciones/:id", (req, res) => {
-    const { id } = req.params
-    const canciones = JSON.parse(fs.readFileSync("repertorio.json"))
-    const index = canciones.findIndex(c => c.id == id)
-    canciones.splice(index, 1)
-    fs.writeFileSync("repertorio.json", JSON.stringify(canciones))
-    res.status(204).send("Canción modificada con éxito")
-/* ;D */
-    res.status(500).json({ error: "Error al procesar la solicitud" })
-    console.error("Error al procesar la solicitud:", error)
-})
-
-app.get("/", (req, res) => {
-    res.status(200).sendFile(__dirname + "/index.html")
-/* ;D por si acaso */
-    res.status(404).sendFile({ error: "Error al procesar la solicitud" })
-    console.error("Error al procesar la solicitud:", error)
-})
-    
+    try {
+        const { id } = req.params;
+        const canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+        const index = canciones.findIndex((c) => c.id == id);
+        canciones.splice(index, 1);
+        fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
+        res.status(204).send("Canción modificada con éxito");
+    } catch (error) {
+        res.status(500).json({ error: "Error al procesar la solicitud" });
+        console.error("Error al procesar la solicitud:", error);
+    }
+});
